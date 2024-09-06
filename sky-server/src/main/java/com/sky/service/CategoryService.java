201e5a4ef2db133a03d55652a7d9719b58a8f7ed
ljl <itljl@itcast.cn>
2024-09-06 17:13:15 +0800

    菜品分类添加与分页查询

---
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.sky.service;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
+
+public interface CategoryService {
+    /**
+     * 新增菜品分类
+     * @param categoryDTO
+     */
+    public void save(CategoryDTO categoryDTO);
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(CategoryPageQueryDTO categoryPageQueryDTO);
+}
